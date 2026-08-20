e0c20682b3a53f6322e0e664f34698a2430f25b8
Miles <15951521585@163.com>
2026-08-20 09:58:16 +0800

    160. 相交链表

--- 160. 相交链表.py
+++ 160. 相交链表.py
@@ -1,12 +1,10 @@
-from typing import Optional
-
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Union[ListNode, None]:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode|None:
         """
         :param headA: 链表A的头节点
         :param headB: 链表B的头节点
